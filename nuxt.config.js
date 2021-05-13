1d4a65991e84d90c66cdea4ab8406fee4dcbb1da
yfhu <879649442@qq.com>
2021-05-13 13:27:46 +0800

    u

--- nuxt.config.js
+++ nuxt.config.js
@@ -30,8 +30,8 @@ export default {
   styleResources: {
     less: ['@/assets/less/colors.less', '@/assets/less/mixin.less']
   },
-  // 模式
-  mode: 'spa',
+  // 模式 spa 没有服务器端渲染（只有客户端路由导航等） universal 服务器端呈现+客户端路由导航等
+  mode: 'universal',
   // 路由配置
   router: {
     middleware: ['i18n'] // 路由中间件
